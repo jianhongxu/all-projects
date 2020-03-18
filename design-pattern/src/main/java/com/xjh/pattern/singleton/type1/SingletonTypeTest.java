fb9563aac6f5a428b4f6ed0581f8f0fd71cbc7d0
xujianhong <jianhong.xu@okchem.com>
2020-03-18 23:50:25 +0800

    代理模式的三种  静态代理 jdk动态代理 cglib动态代理

--- design-pattern/src/main/java/com/xjh/pattern/singleton/type1/SingletonTypeTest.java
+++ design-pattern/src/main/java/com/xjh/pattern/singleton/type1/SingletonTypeTest.java
@@ -8,6 +8,10 @@ package com.xjh.pattern.singleton.type1;
  * 可能造成内存浪费， 因为是类变量 在类加载的时候就初始化好对象 所以不存在线程安全问题
  * 不能进行懒加载
  * 对于类的初始化 触发条件有很多
+ *
+ *
+ * 单例模式的使用场景：
+ *  需要频繁创建和销毁的对象，创建对象耗时过多或者耗费资源过多  比如经常用到的对象、工具类对象、数据源、 session工厂；
  */
 public class SingletonTypeTest {
 
